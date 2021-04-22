ba97cfe0f0e71a97e37eb6a5153cb4aae61c08ac
RBin1630 <932969921@qq.com>
2021-04-22 19:59:56 +0800

    部署到VERCEL

---
+++ bin/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  qq: 'o1152921504752039972',  // 如果是微信号的，这里写 wxuin，可以在浏览器端登录后 cookie 中获取
+  // wxuin: 'o1152921504752039972',
+  port: 3300,
+  useDataStatistics: false, // 是否启用数据统计和ip拦截
+}
